78703688dd2ab77799e413b38ec742528514c12c
Brandon Marrero <brandonmar97@gmail.com>
2020-03-23 15:06:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,8 @@ class Song
     row = filename
     
     data = row.split(" - ")
-    artist_name = data[0].gsub!(".mp3", "")
-    song_name = data[1]
+    artist_name = data[0]
+    song_name = data[1].gsub(".mp3", "")
     
     
     song = self.new 
